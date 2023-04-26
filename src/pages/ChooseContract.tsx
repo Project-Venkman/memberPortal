aceacc2fc359c7bd29c5bea4ede8747a4f091788
Kalvin <Kalvin@venkmanholdings.com>
2023-04-26 14:32:32 -0500

    Added images styled WIP

--- src/pages/ChooseContract.tsx
+++ src/pages/ChooseContract.tsx
@@ -1,33 +1,46 @@
 import React, { useState } from "react";
-
+import Bill from "@assets/bill/Bill-Murray-Applied-at-P.F.-Changs-scaled.jpg";
+// import Earth from "@assets/earth/earth.jpg";
+import ELF from "@assets/images/rkt164.jpg"
 interface Contract {
     title: string;
     description: string;
+    image: string;
 }
 
 const ContractCard: React.FC<{
     title: string;
     description: string;
+    image: string;
     isExpanded: boolean;
     onExpand: () => void;
-}> = ({ title, description, isExpanded, onExpand }) => {
+}> = ({ title, description, image, isExpanded, onExpand }) => {
     return (
         <div
-            className={`p-4 border border-gray-300 rounded-md flex flex-col ${isExpanded ? "h-96" : "h-96"
+            className={`p-4 border border-gray-300 rounded-md flex ${isExpanded ? "h-96 w-full" : "h-96 w-[200px]"
                 }`}
             onClick={onExpand}
         >
-            <h3 className="text-lg font-medium mb-2">{title}</h3>
-            {isExpanded && <p>{description}</p>}
+            <div className="flex flex-row max-w-[100%]">
+                <div className="flex-grow">
+                    <h3 className="text-lg text-center font-medium mb-8">{title}</h3>
+                    <img className="w-[150px]" src={image} alt={title} />
+                </div>
+                {isExpanded && (
+                    <div className="flex flex-col flex-grow">
+                        <h3 className="text-lg text-center font-medium ml-8 mb-8">Description</h3>
+                        <p className="ml-8 mb-4">{description}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
 
 const ChooseContract: React.FC = () => {
     const contracts: Contract[] = [
-        { title: "Bill Murray", description: "Description of Contract 1" },
-        { title: "EarthLight", description: "Description of Contract 2" },
-        // { title: "Contract 3", description: "Description of Contract 3" },
+        { title: "Bill Murray", description: "Description of Contract 1", image: Bill },
+        { title: "EarthLight", description: "Description of Contract 2", image: ELF },
     ];
 
     const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
@@ -51,6 +64,7 @@ const ChooseContract: React.FC = () => {
                             <ContractCard
                                 title={contract.title}
                                 description={contract.description}
+                                image={contract.image}
                                 isExpanded={index === expandedIndex}
                                 onExpand={() => handleExpand(index)}
                             />
