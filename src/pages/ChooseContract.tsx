a813d3f4cf3de171f6dd037d73e689fc1efdf5e6
Kalvin <Kalvin@venkmanholdings.com>
2023-04-27 11:34:32 -0500

    Working just need to add content now

--- src/pages/ChooseContract.tsx
+++ src/pages/ChooseContract.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-
+import React, { useState } from "react";
 interface AccordionData {
     title: string;
     description: string;
@@ -11,21 +10,21 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ data }) => {
-    const [activeIndex, setActiveIndex] = useState<number | null>(null)
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
     const toggleAccordion = (index: number) => {
         if (activeIndex === index) {
-            setActiveIndex(null)
+            setActiveIndex(null);
         } else {
-            setActiveIndex(index)
+            setActiveIndex(index);
         }
-    }
+    };
 
     return (
-        <div className="flex flex-grow">
-            {data.map((item, index) => (
+        <div className="flex w-full">
+            {data.map((item, index) => [
                 <div
-                    key={index}
+                    key={`item-${index}`}
                     className="border-b flex flex-row min-w-[200px]"
                     style={{
                         backgroundImage: `url(${item.image})`,
@@ -38,15 +37,15 @@ const Accordion: React.FC<AccordionProps> = ({ data }) => {
                     <div className="flex mx-auto items-center justify-between px-4 py-3 cursor-pointer">
                         <h3 className="text-lg font-medium text-white">{item.title}</h3>
                     </div>
-                    {activeIndex === index && (
-                        <div className="px-4 py-3 bg-black flex-grow">
-                            <p className="text-sm text-gray-700">{item.description}</p>
-                        </div>
-                    )}
-                </div>
-            ))}
+                </div>,
+                activeIndex === index && (
+                    <div key={`desc-${index}`} className="px-4 py-3 bg-black flex-grow">
+                        <p className="text-sm text-gray-700">{item.description}</p>
+                    </div>
+                ),
+            ])}
         </div>
-    )
-}
+    );
+};
 
-export default Accordion
+export default Accordion;
