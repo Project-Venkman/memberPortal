dd07a49941ef87e8c06c53b4c8d8e964f4aaf924
Kalvin <Kalvin@venkmanholdings.com>
2023-05-25 17:26:38 -0500

    image and loading fix

--- src/components/Result/ItemAssetImage.tsx
+++ src/components/Result/ItemAssetImage.tsx
@@ -7,36 +7,31 @@ import loadingGif from "@assets/images/loading2.gif";
 import { Metadata } from "@customtypes/HyperMint";
 
 export const ItemAssetImage: React.FC<ItemProps> = (props) => {
-  const {} = props;
-  const asset: Asset = useSelector((state: RootState) => state.currentAsset);
-  const oAsset: Metadata = useSelector((state: RootState) => state.currentOwnedAsset);
-  let currentAsset: Asset = useSelector((state: RootState) => state.currentAsset);
+	const { } = props;
+	const asset: Asset = useSelector((state: RootState) => state.currentAsset);
+	const oAsset: Metadata = useSelector((state: RootState) => state.currentOwnedAsset);
+	let currentAsset: Asset = useSelector((state: RootState) => state.currentAsset);
 
-  const [loaded, setLoaded] = useState<boolean>(false);
-  const counter = useRef(0);
-  const onLoad = () => {
-    setLoaded(true);
-  };
+	const [loaded, setLoaded] = useState<boolean>(false);
+	const counter = useRef(0);
+	const onLoad = () => {
+		setLoaded(true);
+	}
 
-  useEffect(() => {}, []);
-  return (
-    <React.Fragment>
-      {currentAsset.imageSmall !== null ? (
-        <NFTImg
-          className={loaded ? "loaded" : ""}
-          src={currentAsset.imageSmall}
-          onLoad={onLoad}
-        />
-      ) : (
-        <NFTImg
-          className={loaded ? "loaded" : ""}
-          src={currentAsset.image}
-          onLoad={onLoad}
-        />
-      )}
-      {/*{!loaded && <NFTImg src={loadingGif}/>}*/}
-    </React.Fragment>
-  );
-};
+	useEffect(() => {
+
+	}, [])
+console.log(oAsset, currentAsset)
+	return (
+		<React.Fragment>
+          <NFTImg
+            className={loaded ? "loaded" : ""}
+            src={currentAsset.imageSmall !== null ? currentAsset.imageSmall : currentAsset.image}
+            onLoad={onLoad}
+ />
+			{/*{!loaded && <NFTImg src={loadingGif}/>}*/}
+		</React.Fragment>
+	)
+}
 
 export default ItemAssetImage;
