21e34f090a0511d9c9521dfceba691123059a819
Kalvin Nguyen <Kalvin@venkmanholdings.com>
2023-06-06 13:35:19 -0500

    WIPGB

--- src/pages/Chive.tsx
+++ src/pages/Chive.tsx
@@ -1,125 +1,83 @@
 import React, { useState } from 'react';
-import { ChiveProps } from '@customtypes/Result';
-import { Web3ModalComponent } from '@components/Wallet';
-import { vBar, Crown } from '@components/Chive';
-import { Api } from '@pages/scripts/API';
+import { GoldBar } from '@components/Chive';
 
-const GoldBar: React.FC<ChiveProps> = (props) => {
-    const [selectedTab, setSelectedTab] = useState(0); // Track the selected tab
-    const [isModalOpen, setIsModalOpen] = useState(false); // Track the modal open/close state
+import { HomeIcon } from '@heroicons/react/24/outline';
 
-    const changeTab = (tabIndex: number) => {
-        setSelectedTab(tabIndex);
+const navigation = [
+    { name: 'ChiveGoldBars', href: '#', icon: HomeIcon, current: true },
+    // { name: 'ChiveCrown', href: '#', icon: UsersIcon, current: false },
+];
+
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ');
+}
+
+const ChiveHome = () => {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [selectedNavigation, setSelectedNavigation] = useState(
+        navigation[0].name
+    );
+
+    const handleNavigationClick = (name: string) => {
+        setSelectedNavigation(name);
     };
 
-    const handleDataClick = () => {
-        setIsModalOpen(true);
+    const renderSelectedComponent = () => {
+        if (selectedNavigation === 'ChiveCrown') {
+            return <div>hello</div>; // Render the desired component for 'ChiveCrown'
+        } else {
+            return <GoldBar />; // Render the GoldBar component for other navigation items
+        }
     };
-
-    const handleSaveClick = () => {
-        Api.googleSheets.SyncDB().then((res) => {
-            console.log(res);
-        });
-    };
-
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
-
-    const tabComponents = [vBar, Crown];
-
-    // Destructure props if needed
-    const {} = props;
 
     return (
-        <div className="container max-w-none h-screen overflow-y-auto mx-auto">
-            <Web3ModalComponent />
-            <div className="flex justify-center h-[10%] items-center text-4xl">
-                <h1 className="text-white">Chive</h1>
-            </div>
-            <div className="h-[20%] flex justify-center items-center">
-                <button
-                    onClick={handleDataClick}
-                    className="sm:w-full md:w-[30%] p-5 border-2 border-white text-white w-[20%] text-center text-2xl hover:bg-white hover:text-black transition-colors duration-300"
-                >
-                    Enter Data
-                </button>
-            </div>
-
-            {/* Modal */}
-            {isModalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="w-[90%] bg-white rounded-lg p-4">
-                        <div className="flex flex-col items-center">
-                            <div className={'w-full flex justify-end'}>
-                                <button
-                                    onClick={() => setIsModalOpen(false)}
-                                    className="relative top-0 right-0 text-gray-500 hover:text-gray-800"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M6 18L18 6M6 6l12 12"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                            <iframe
-                                src="https://docs.google.com/spreadsheets/d/1qCvb6r50PxTbnHbv4FFnfctGezR5pHnlQZqU9R9Wdcs/edit#gid=0?usp=sharing?&amp;rm=minimal&amp;single=true&amp;"
-                                width={'90%'}
-                                height="600"
-                            ></iframe>
+        <>
+            <div className="flex h-screen overflow-hidden">
+                {/* Sidebar */}
+                <div className="flex flex-col w-24 bg-gray-900">
+                    <div className="flex flex-col items-center mt-4">
+                        {navigation.map((item) => (
                             <button
-                                // onClick={() => setIsModalOpen(false)}
-                                onClick={handleSaveClick}
-                                className="mt-4 min-w-[100px] p-2 text-white text-xl bg-indigo-500 rounded hover:bg-indigo-600 transition-colors duration-300"
+                                key={item.name}
+                                className={classNames(
+                                    'w-full py-4 flex flex-col items-center justify-center text-gray-400 hover:text-white',
+                                    item.current && 'text-white'
+                                )}
+                                onClick={() => handleNavigationClick(item.name)}
                             >
-                                Finish
+                                <item.icon
+                                    className="w-6 h-6"
+                                    aria-hidden="false"
+                                />
+                                <span className="text-xs mt-1">
+                                    {item.name}
+                                </span>
                             </button>
-                        </div>
+                        ))}
                     </div>
                 </div>
-            )}
 
-            <div className="flex flex-col items-center h-full">
-                <div className="flex flex-col h-full w-full bg-gray-900">
-                    <div className="space-x-4">
-                        <button
-                            className={`text-sm text-white border-b-2 border-transparent hover:border-indigo-500 focus:border-indigo-500 focus:outline-none py-2 px-4 ${
-                                selectedTab === 0
-                                    ? 'bg-indigo-500 text-white'
-                                    : ''
-                            }`}
-                            onClick={() => changeTab(0)}
-                        >
-                            VBar
-                        </button>
-                        <button
-                            className={`text-sm text-white border-b-2 border-transparent hover:border-indigo-500 focus:border-indigo-500 focus:outline-none py-2 px-4 ${
-                                selectedTab === 1
-                                    ? 'bg-indigo-500 text-white'
-                                    : ''
-                            }`}
-                            onClick={() => changeTab(1)}
-                        >
-                            Crown
-                        </button>
+                {/* Content */}
+                <div className="flex flex-col flex-grow">
+                    {/* Mobile Navbar */}
+                    <div className="lg:hidden">{/* ... */}</div>
+
+                    {/* Desktop Navbar */}
+                    <div className="hidden lg:flex lg:items-center lg:justify-between bg-black px-4 py-4">
+                        {/* ... */}
                     </div>
-                    <div className="mt-4 overflow-y-auto" id="tabContent">
-                        {tabComponents[selectedTab]()}
-                    </div>
+
+                    {/* Main Content */}
+                    <main className="">
+                        <div className="px-4 sm:px-6 lg:px-8">
+                            {renderSelectedComponent()}{' '}
+                            {/* Render the selected component */}
+                        </div>
+                    </main>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
-export default GoldBar;
+export default ChiveHome;
